37c8e0a0d735be3f3eb7c5c52e147e6862e5063d
Hessel Bakker <h.bakkerit@gmail.com>
2018-03-23 17:29:56 +0100

    initial commit

---
+++ useful_snippets.sh
@@ -0,0 +1,19 @@
+# awesome shebang
+#!/usr/bin/env bash
+
+
+# check if user is root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 
+  exit 1
+fi
+
+# check if bash version is greater than 4 (useful for some commands)
+if [[ "${BASH_VERSINFO:-0}" -lt 4 ]]; then
+   echo "This script must be run in a bash shell of version 4.0 or higher" 
+   exit 1
+fi
+
+# collect user input
+read -p 'fully qualified domain name: ' fqdn
+echo $fqdn
