110ab8c02f6493be8601a4e1472b5442fd892476
Hessel Bakker <h.bakkerit@gmail.com>
2018-03-26 11:51:01 +0200

    initial commit

---
+++ apt.sh
@@ -0,0 +1,7 @@
+# useful apt-get stuff
+
+## Simulate: run a simulation to see what would happen if you upgrade/install a package
+apt-get -s install <package>
+
+## show version information about one or more packages
+apt-show-versions <package>
