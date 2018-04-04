199459d9d756d1f3b3c8b7fe813d1da60ef2c82a
Hessel Bakker <h.bakkerit@gmail.com>
2018-04-04 10:13:35 +0200

    corrected commands and added destroy command

--- terraform/terraform_usage.sh
+++ terraform/terraform_usage.sh
@@ -1,11 +1,17 @@
 terraform plan \
   -var "do_token=${DO_PAT}" \
-  -var "pub_key=$HOME/.ssh/id_ed25519.pub" \
-  -var "pvt_key=$HOME/.ssh/id_ed25519" \
-  -var "ssh_fingerprint=$SSH_FINGERPRINT"
+  -var "pub_key=${HOME}/.ssh/id_ed25519.pub" \
+  -var "pvt_key=${HOME}/.ssh/id_ed25519" \
+  -var "ssh_fingerprint=${SSH_FINGERPRINT}"
 
 terraform apply \
   -var "do_token=${DO_PAT}" \
-  -var "pub_key=$HOME/.ssh/id_ed25519.pub" \
-  -var "pvt_key=$HOME/.ssh/id_ed25519" \
-  -var "ssh_fingerprint=$SSH_FINGERPRINT"
+  -var "pub_key=${HOME}/.ssh/id_ed25519.pub" \
+  -var "pvt_key=${HOME}/.ssh/id_ed25519" \
+  -var "ssh_fingerprint=${SSH_FINGERPRINT}"
+
+terraform destroy \
+  -var "do_token=${DO_PAT}" \
+  -var "pub_key=${HOME}/.ssh/id_ed25519.pub" \
+  -var "pvt_key=${HOME}/.ssh/id_ed25519" \
+  -var "ssh_fingerprint=${SSH_FINGERPRINT}"
