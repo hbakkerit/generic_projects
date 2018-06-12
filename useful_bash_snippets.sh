94f37cc55a9ea5bb60b823c434cd6e9961668e10
Hessel Bakker <h.bakkerit@gmail.com>
2018-06-12 08:33:18 +0000

    updated

--- useful_bash_snippets.sh
+++ useful_bash_snippets.sh
@@ -2,11 +2,11 @@
 #!/usr/bin/env bash
 
 
-# check if user is root
-if [[ $EUID -ne 0 ]]; then echo "This script must be run as root"; exit 1; fi
+# checks
+if [[ ${EUID} -ne 0 ]]; then echo "This script must be run as root"; exit 1; fi # checks whether the user is root
+if [[ "${BASH_VERSINFO:-0}" -lt 4 ]]; then echo "This script must be run in a bash shell of version 4.0 or higher"; exit 1; fi # checks whether the bash version used is 4.0 or higher
+if [[ ${distro} != ${intended_distro} ]]; then echo "This script should run on the Ubuntu distribution"; exit 1; fi # checks whether the distro is Ubuntu
 
-# check if bash version is greater than 4 (useful for some commands)
-if [[ "${BASH_VERSINFO:-0}" -lt 4 ]]; then echo "This script must be run in a bash shell of version 4.0 or higher"; exit 1; fi
 
 # collect user input
 read -p 'fully qualified domain name: ' fqdn
