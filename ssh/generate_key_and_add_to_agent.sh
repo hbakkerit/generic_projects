b0579e71fd46776a4fd26361d0f2ed9b8c3b3949
Hessel Bakker <h.bakkerit@gmail.com>
2018-06-12 15:19:58 +0000

    added some comments and small changes

--- ssh/generate_key_and_add_to_agent.sh
+++ ssh/generate_key_and_add_to_agent.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-cd ~/.ssh
+if [[ ${EUID} -ne 0 ]]; then echo "This script must be run as root"; exit 1; fi # checks whether the user is root
+
+cd ~/.ssh 
+eval `ssh-agent -s` # starts ssh-agent process
 read -p 'filename of the key file: ' filename
-ssh-keygen -o -a 100 -t ed25519 -f ${filename}
+ssh-keygen -o -a 100 -t ed25519 -f ${filename} # generates ed25519 key
+chmod 400 ${filename}
 ssh-add ${filename}
