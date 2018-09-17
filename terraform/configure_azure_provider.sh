a0880d04e004bbb9c41174bc139828e9ba2892ba
Hessel Bakker <h.bakkerit@gmail.com>
2018-09-17 11:56:57 +0200

    typo

--- terraform/configure_azure_provider.sh
+++ terraform/configure_azure_provider.sh
@@ -2,5 +2,5 @@
 echo "Setting Azure provider environment variables for Terraform..."
 read -e -p 'please input your Azure Subscription ID: '  ARM_SUBSCRIPTION_ID 
 read -e -p 'please input your Azure Client ID: '  ARM_CLIENT_ID 
-read -e -p 'please input your Azure Client ID: '  ARM_CLIENT_SECRET 
+read -e -p 'please input your Azure Client Secret/Password: '  ARM_CLIENT_SECRET 
 read -e -p 'please input your Azure Tenant ID: '  ARM_TENANT_ID 
