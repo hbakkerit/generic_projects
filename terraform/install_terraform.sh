c41470a8385807f40430a9f4a9f0e20cab722135
Hessel Bakker <h.bakkerit@gmail.com>
2018-09-17 11:56:45 +0200

    moved generic config to install_terraform.sh script

--- terraform/install_terraform.sh
+++ terraform/install_terraform.sh
@@ -5,19 +5,16 @@ TERRAFORM_VERSION=0.11.8
 TERRAFORM_ARCHITECTURE=amd64
 TERRAFORM_HOME=/opt/terraform
 TEMP_INSTALL_DIR=/tmp
+TERRAFORM_CONFIG_SUBDIR=config
 
 if [[ ${distro} != ${intended_distro} ]]; then echo "This script should run on the Ubuntu distribution"; exit 1; fi # checks whether the distro is Ubuntu
 if [[ ${EUID} -ne 0 ]]; then echo "This script must be run as root"; exit 1; fi # checks whether the user is root
 
 # install basic packages
-echo "Updating packages..."
-apt-get update -y
-echo "Installing basic packages..."
-apt-get install -y wget unzip
-
+echo "Updating packages..." && apt-get update -y
+echo "Installing basic packages..." && apt-get install -y git wget unzip
 
 # install terraform
-
 ## removing old versions
 if [ -d "$TERRAFORM_HOME" ]; then
   rm -rf $TERRAFORM_HOME
@@ -28,7 +25,15 @@ unzip -o ${TEMP_INSTALL_DIR}/terraform_${TERRAFORM_VERSION}_linux_${TERRAFORM_AR
 export PATH=${PATH}:${TERRAFORM_HOME}
 echo PATH=\"${PATH}\" > /etc/environment
 
-# creating a tag
+# pulling the config
+echo "This script assumes an existing terraform configuration repository (git), containing .tf terraform configuration files"
+read -e -p 'please input your Terraform configuration repository (git): ' TERRAFORM_CONFIG_REPO
+
+git clone ${TERRAFORM_CONFIG_REPO} ${TERRAFORM_HOME}/${TERRAFORM_CONFIG_SUBDIR}
+cd ${TERRAFORM_HOME}/${TERRAFORM_CONFIG_SUBDIR} && terraform init
+echo "Install script finished, go to ${TERRAFORM_HOME}/${TERRAFORM_CONFIG_SUBDIR} to find your configuration"
+
+## creating a tag
 grep -q -F "APP=Terraform" /etc/environment || echo "APP=Terraform" >> /etc/environment
 
 # verify
