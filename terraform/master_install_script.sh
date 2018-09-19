f9b55086a5edf508ffa9449f62d25dae33b358ea
Hessel Bakker <h.bakkerit@gmail.com>
2018-09-19 13:23:33 +0200

    added if statements and corrected script names

--- terraform/master_install_script.sh
+++ terraform/master_install_script.sh
@@ -5,7 +5,7 @@ if [[ ${EUID} -ne 0 ]]; then echo "This script must be run as root"; exit 1; fi 
 
 echo "Prerequisites: "
 echo "- an existing terraform configuration repository (git), containing .tf terraform configuration files"
-echo "- a Digital Ocean Personal Access Token"
+echo "- a Personal Access Token or API key for the service provider you are trying to use (Azure/AWS/etc)"
 echo ""
 echo "Press any key to continue.."
 read
@@ -19,5 +19,18 @@ chmod 750 ./*.sh
 # generate ssh key
 ./generate_ssh_key.sh
 
+read -p "Do you want to configure Digital Ocean as a provider for Terraform? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
 # configure terraform for digital ocean
-./configure_terraform_for_digital_ocean.sh
+./configure_digital_ocean_provider.sh
+fi
+
+read -p "Do you want to configure Azure as a provider for Terraform? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+# configure terraform for digital ocean
+./configure_azure_provider.sh
+fi
